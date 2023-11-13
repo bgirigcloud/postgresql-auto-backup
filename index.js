7cd7f54bc82afb9dad3ad9e3a140aff8c392457a
bgiri_gcloud <49786519+bgirigcloud@users.noreply.github.com>
2023-11-13 13:30:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const {google} = require("googleapis");
 const {auth} = require("google-auth-library");
 var sqladmin = google.sqladmin("v1beta4");
@@ -11,27 +10,27 @@ exports.exportDatabase = (_req, res) => {
       // Project ID of the project that contains the instance to be exported.
       project:"kinetic-object-400913",
       // Cloud SQL instance ID. This does not include the project ID.
-      instance: "demo-sql-server",
+      instance: "demo-postgresql",
       resource: {
         // Contains details about the export operation.
         exportContext: {
           // This is always sql#exportContext.
           // The file type for the specified uri (e.g. SQL or CSV)
-          fileType: "BAK", // CSV
+          fileType: "SQL", // CSV
           /**
            * The path to the file in GCS where the export will be stored.
            * The URI is in the form gs://bucketName/fileName.
            * If the file already exists, the operation fails.
            * If fileType is SQL and the filename ends with .gz, the contents are compressed.
            */
-          uri: `gs://kinetic-object-400913-sql-backup/backup-${Date.now()}.gz`,
+          uri:`gs://kinetic-object-400913-postgresql-demo-auto-backup/backup-${Date.now()}.gz`,
           /**
            * Databases from which the export is made.
            * If fileType is SQL and no database is specified, all databases are exported.
            * If fileType is CSV, you can optionally specify at most one database to export.
            * If csvExportOptions.selectQuery also specifies the database, this field will be ignored.
            */
-          databases:["demo-sqlserver"]
+          databases:["postgres"]
    
           // Options for exporting data as SQL statements.
           // sqlExportOptions: {
@@ -62,3 +61,4 @@ exports.exportDatabase = (_req, res) => {
   doIt();
 };
  
+
